7c1d8ba5b7389294ccea082cb921ab7e3e8c2366
rmkkmr17 <ramkum94@tutanota.com>
2023-01-14 13:36:41 +0100

    Added book constructor and DOM elements to display the books

--- script.js
+++ script.js
@@ -5,19 +5,38 @@ function Book(name, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-    if (read === true) {
-      return `${name} by ${author}, ${pages} pages, I have already read it`;
-    } else {
-      return `${name} by ${author}, ${pages} pages, not read yet`;
+  // this.info = function() {
+  //   if (read === true) {
+  //     return `${name} by ${author}, ${pages} pages, I have already read it`;
+  //   } else {
+  //     return `${name} by ${author}, ${pages} pages, not read yet`;
+  //   }
+  // }
+}
+
+function addBookToLibrary(name, author, pages, read) {
+  let book = new Book(name, author, pages, read);
+  myLibrary.push(book)
+}
+
+addBookToLibrary("Avanti, Jeeves!", "P.G. Wodehouse", 235, true)
+addBookToLibrary("Davanti, Jeeves!", "P.G. Wodehouse", 265, true)
+
+
+const displayBooks = () => {
+  const cardContainer = document.querySelector(".card-container");
+  for (let book of myLibrary) {
+    const card = document.createElement("div");
+    for (let key in book) {
+      console.log(book[key])
+      let para = document.createElement("p");
+      para.classList.add("para")
+      para.textContent = `${key}: ` + book[key]
+      card.appendChild(para);
     }
+    card.classList.add("card")
+    cardContainer.appendChild(card);
   }
 }
 
-const theHobbit = new Book("The Hobbit", "Tolkien", 296, false)
-
-console.log(theHobbit.info())
-
-function addBookToLibrary() {
-  // do stuff here
-}
+displayBooks();

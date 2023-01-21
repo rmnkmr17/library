1f0627c662de1ab01ace4dc9dd10d693df2fa736
rmkkmr17 <ramkum94@tutanota.com>
2023-01-21 16:38:39 +0100

    New Book button to add books

--- script.js
+++ script.js
@@ -5,38 +5,61 @@ function Book(name, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.info = function() {
-  //   if (read === true) {
-  //     return `${name} by ${author}, ${pages} pages, I have already read it`;
-  //   } else {
-  //     return `${name} by ${author}, ${pages} pages, not read yet`;
-  //   }
-  // }
 }
 
 function addBookToLibrary(name, author, pages, read) {
-  let book = new Book(name, author, pages, read);
+  const book = new Book(name, author, pages, read);
   myLibrary.push(book)
+  displayBooks()
+  
 }
 
-addBookToLibrary("Avanti, Jeeves!", "P.G. Wodehouse", 235, true)
-addBookToLibrary("Davanti, Jeeves!", "P.G. Wodehouse", 265, true)
-
-
 const displayBooks = () => {
-  const cardContainer = document.querySelector(".card-container");
+  
+    
+  const divsToRemove = document.querySelector(".card");
+  if (divsToRemove !== null) {
+
+    divsToRemove.remove();
+  }
   for (let book of myLibrary) {
+    const cardContainer = document.querySelector(".card-container");
     const card = document.createElement("div");
+    card.classList.add("card");
+    cardContainer.append(card)
     for (let key in book) {
-      console.log(book[key])
       let para = document.createElement("p");
-      para.classList.add("para")
-      para.textContent = `${key}: ` + book[key]
-      card.appendChild(para);
+      para.textContent = key.charAt(0).toLocaleUpperCase() + key.slice(1) + ": " + book[key];
+      card.append(para);
     }
-    card.classList.add("card")
-    cardContainer.appendChild(card);
   }
 }
 
-displayBooks();
+// Modal
+
+const modal = document.querySelector(".modal")
+const newBookButton = document.querySelector(".new-book");
+const closeModalButton = document.querySelector(".close");
+
+newBookButton.addEventListener("click", () => {
+  modal.showModal()
+})
+closeModalButton.addEventListener("click", () => {
+  modal.close();
+})
+
+// Adding book on the display
+
+const addBook = () => {
+  const name = document.getElementById("name").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+  addBookToLibrary(name, author, pages, read);
+  document.getElementById("form").reset();
+}
+
+const addBookButton = document.querySelector(".add");
+addBookButton.addEventListener("click", addBook)
+
+console.log(displayBooks())

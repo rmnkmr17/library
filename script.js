3e5f173ffa62206ee1e27cad4557bbdb25384f04
rmkkmr17 <ramkum94@tutanota.com>
2023-01-22 18:02:32 +0100

    Added the delete button and the change status button

--- script.js
+++ script.js
@@ -8,30 +8,64 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary(name, author, pages, read) {
-  const book = new Book(name, author, pages, read);
+  let book = new Book(name, author, pages, read);
   myLibrary.push(book)
   displayBooks()
-  
 }
 
 const displayBooks = () => {
   
-    
-  const divsToRemove = document.querySelector(".card");
-  if (divsToRemove !== null) {
-
-    divsToRemove.remove();
+  // remove divs
+  const divsToRemove = document.querySelectorAll(".card");
+  for (let div of divsToRemove) {
+    div.remove();
   }
+  
+  // loop through books
   for (let book of myLibrary) {
     const cardContainer = document.querySelector(".card-container");
     const card = document.createElement("div");
     card.classList.add("card");
-    cardContainer.append(card)
+    cardContainer.append(card);
+    
     for (let key in book) {
       let para = document.createElement("p");
+      para.classList.add(`${key}`)
       para.textContent = key.charAt(0).toLocaleUpperCase() + key.slice(1) + ": " + book[key];
       card.append(para);
     }
+
+    // delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("data-index", myLibrary.indexOf(book));
+    deleteButton.textContent = "Delete";
+    card.append(deleteButton);
+    deleteButton.addEventListener("click", deleteBookFromLibrary);
+    
+    // change status
+    let changeStatusButton = document.createElement("button");
+    changeStatusButton.classList.add("change-status");
+    changeStatusButton.setAttribute("data-index", myLibrary.indexOf(book));
+    changeStatusButton.textContent = "Change Status";
+    changeStatusButton.addEventListener("click", changeStatus);
+    card.append(changeStatusButton);
+  }
+}
+
+function deleteBookFromLibrary() {
+  const index = this.getAttribute("data-index");
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function changeStatus() {
+  let index = this.getAttribute("data-index");
+  if (myLibrary[index].read === "No") {
+    myLibrary[index].read = "Yes";
+    displayBooks();
+  } else {
+    myLibrary[index].read = "No";
+    displayBooks()
   }
 }
 
@@ -50,16 +84,30 @@ closeModalButton.addEventListener("click", () => {
 
 // Adding book on the display
 
-const addBook = () => {
+const addBook = (e) => {
+  e.preventDefault()
   const name = document.getElementById("name").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").value;
+  let read = document.getElementById("read");
+
+  if (read.checked === true) {
+    read = "Yes";
+  } else  {
+    read = "No";
+  }
+
+  if (name === "" || author === "" || pages === "") {
+    return;
+  }
+
   addBookToLibrary(name, author, pages, read);
   document.getElementById("form").reset();
+  modal.close()
 }
 
 const addBookButton = document.querySelector(".add");
 addBookButton.addEventListener("click", addBook)
+
 
 console.log(displayBooks())
